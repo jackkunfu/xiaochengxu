64837b841b7d26a7a85e3267b7e05eea3e8255fb
wadaa <632792338@qq.com>
2018-07-05 10:54:55 +0800

    gx

--- mongo/pages/photo/photo.js
+++ mongo/pages/photo/photo.js
@@ -17,21 +17,46 @@ Page({
         this.getList()
     },
 
+    delPhoto(v){
+        console.log(v.currentTarget.dataset)
+        console.log(v.currentTarget.dataset)
+        var url = v.currentTarget.dataset.url
+        REQ.result({
+            url: '/userphoto/delete', //替换自己的本地服务器地址
+            data: {
+                photoUrl: url
+            },
+            method: 'post'
+        }).then(res => {
+            this.getList()
+        })
+    },
+
     getList(){
         REQ.result({
-            url: REQ.baseUrl + '/user/userTokenByPhoto', //替换自己的本地服务器地址
-            data: {}
+            url: '/userphoto/userTokenByPhoto', //替换自己的本地服务器地址
+            data: {},
+            method: 'get'
         }).then(res => {
-
+            this.setData({
+                list: res.data.data
+            })
         })
     },
 
     upfile(){
+        var allList = this.data.list
+        if(allList.length == 6){
+            wx.showToast({
+                title: '最多只能6张',
+            })
+            return
+        }
         wx.chooseImage({
             success: (res) => {
                 var tempFilePaths = res.tempFilePaths
                 wx.uploadFile({
-                    url: REQ.baseUrl+'/pic/upload',
+                    url: REQ.baseUrl +'/pic/upload',
                     filePath: tempFilePaths[0],
                     name: 'file',
                     formData: {
@@ -40,16 +65,20 @@ Page({
                     success:  (res) => {
                         var data = res.data ? JSON.parse(res.data).data : ''
                         if(!data) return
-                        var list = this.data.list
-                        list.push(data)
-                        this.setData({
-                            list
-                        })
+                        
+                        console.log(this.data.list)
+                        var arr = []
+                        arr.push(data)
                         REQ.result({
-                            url: REQ.baseUrl+'/userphoto/save/photo', //替换自己的本地服务器地址
-                            data: [data]
+                            url: '/userphoto/save/photo', //替换自己的本地服务器地址
+                            data: {
+                                data: JSON.stringify(arr)
+                            }
                         }).then(res => {
-
+                            allList.push(data)
+                            this.setData({
+                                list: allList
+                            })
                         })
                         //do something
                     }
